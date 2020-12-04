ebd58f4f81c839c7ed697cf042d61a3f5b885f6f
slittleton <littletonltp@gmail.com>
2020-12-04 10:18:28 -0700

    added service maps for owner, pet, and vet

---
+++ pet-clinic/pet-clinic-data/src/main/java/com/spring/model/map/OwnerServiceMap.java
@@ -0,0 +1,39 @@
+package com.spring.model.map;
+
+import java.util.Set;
+
+import com.spring.model.Owner;
+import com.spring.services.CrudService;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+	@Override
+	public Set<Owner> findAll() {
+
+		return  super.findAll();
+	}
+
+	@Override
+	public Owner findById(Long id) {
+
+		return  super.findById(id);
+	}
+
+	@Override
+	public Owner save(Owner object) {
+
+		return  super.save(object.getId(), object);
+	}
+
+	@Override
+	public void delete(Owner object) {
+
+		 super.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+}
--- pet-clinic/pet-clinic-data/src/main/java/com/spring/services/CrudService.java
+++ pet-clinic/pet-clinic-data/src/main/java/com/spring/services/CrudService.java
@@ -3,13 +3,13 @@ package com.spring.services;
 import java.util.Set;
 
 public interface CrudService<T, ID> {
-	Set<T> findAll();
+    Set<T> findAll();
 
-	T findById(ID id);
+    T findById(ID id);
 
-	T save(T object);
-	
-	void delete(T object);
-	
-	void deleteById(ID id);
+    T save(T object);
+
+    void delete(T object);
+
+    void deleteById(ID id);
 }
