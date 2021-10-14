ec837cbe3d8cf4f48e941745dd5ef64c9164e31c
slittleton <littletonltp@gmail.com>
2021-10-14 10:36:47 -0700

    updated inheritance on model classes

--- pet-clinic-data/src/main/java/com/example/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/Owner.java
@@ -1,4 +1,4 @@
 package com.example.petclinic.model;
 
-public class Owner {
+public class Owner extends Person{
 }
--- pet-clinic-data/src/main/java/com/example/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/Vet.java
@@ -1,4 +1,4 @@
 package com.example.petclinic.model;
 
-public class Vet {
+public class Vet extends Person{
 }
