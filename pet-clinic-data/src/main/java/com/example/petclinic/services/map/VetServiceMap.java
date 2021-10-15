3cb2f4018f318c665541986f7ca1d4f805687a7b
slittleton <littletonltp@gmail.com>
2021-10-15 15:10:28 -0700

    added AbstractMap Service

---
+++ pet-clinic-data/src/main/java/com/example/petclinic/services/map/VetServiceMap.java
@@ -0,0 +1,35 @@
+package com.example.petclinic.services.map;
+
+
+import com.example.petclinic.model.Vet;
+import com.example.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findByID(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+}
